b8509a1d85dfca1cbefe808df86c89abebc283f3
7thFox <dev7thFox@gmail.com>
2022-02-08 21:12:25 -0600

    make tests a lot less awful

--- src/entrypoints/test.c
+++ src/entrypoints/test.c
@@ -1,7 +1,7 @@
 #include "stdio.h"
 #include "signal.h"
 #include "time.h"
-#include "ncurses.h"
+#include "stdlib.h"
 #include "execinfo.h"
 #include "../headers/log.h"
 #include "../headers/ram.h"
@@ -9,111 +9,282 @@
 #include "../headers/cpu6502.h"
 #include "../headers/disasm.h"
 
+#define MAX_CYCLES_PER_OP 6
+#define RAM_OFFSET 0x0000
+#define ROM_OFFSET 0x4000
+
+char error_message[256];// I'd prefer not to malloc/free for every test
+
 #define testcase(name) TestResult name()
-#define assert(cond, msg)                                     \
-    if (!(cond))                                              \
-    {                                                         \
-        char *msg_dyn = malloc(sizeof(msg) / sizeof(msg[0])); \
-        strcpy(msg_dyn, msg);                                 \
-        return (TestResult){                                  \
-            is_success : false,                               \
-            failed_message : msg_dyn,                         \
-        };                                                    \
-    }
 
 typedef struct {
     bool is_success;
-    char *failed_message;
 } TestResult;
 
-#define setup_cpu(rom_value)                           \
-    Cpu6502 cpu;                                       \
-    {                                                  \
-        MemoryMap mem;                                 \
-        mem.n_read_blocks = 0;                         \
-        mem.n_write_blocks = 0;                        \
-        mem._ppu = NULL;                               \
-                                                       \
-        Rom rom;                                       \
-        rom.value = rom_value;                         \
-        rom.rom_size = sizeof(rom_value) / sizeof(u8); \
-        rom.map_offset = 0x0000;                       \
-        mem_add_rom(&mem, &rom, "ROM");                \
-                                                       \
-        cpu.memmap = &mem;                             \
-        cpu.addr_bus = rom.map_offset;                 \
-        cpu_resb(&cpu);                                \
-        cpu.pc = 0x0000;                               \
-        cpu.addr_bus = 0x0000;                         \
+typedef struct {
+    // general information
+    int num_cycles;
+
+    // initial values
+    u16 pc0;
+    u8 x0;
+    u8 y0;
+    u8 a0;
+    u8 sp0;
+    u8 p0;
+
+    // resulting values
+    u16 pc1;
+    u8 x1;
+    u8 y1;
+    u8 a1;
+    u8 sp1;
+    u8 p1;
+    u8 bit_fields1;
+    u16 addr_bus1;
+
+} InstructionExecutionInfo;
+
+typedef struct {
+    int num_cycles;
+    int instruction_size;
+
+    u8 flags_set;
+    u8 flags_unset;
+
+    bool updates_x;
+    bool updates_y;
+    bool updates_a;
+    bool updates_sp;
+    u8 x;
+    u8 y;
+    u8 a;
+    u8 sp;
+} ExpectedExecutionInfo;
+
+#define assert_equals(expected, actual, value_name)         \
+    if ((actual) != (expected))                             \
+    {                                                       \
+        sprintf(error_message,                              \
+                "%s: Expected $%02x (%i), got $%02x (%i).", \
+                value_name,                                 \
+                expected, expected,                         \
+                actual, actual);                            \
+        return (TestResult){is_success : false};            \
     }
+
+InstructionExecutionInfo execute_instruction(
+    u8 *rom_value, size_t rom_size,
+    u8 *ram_value, size_t ram_size,
+    void(*pre_execute)(Cpu6502*))
+{
+    MemoryMap mem;
+    mem.n_read_blocks = 0;
+    mem.n_write_blocks = 0;
+    mem._ppu = NULL;
+
+    Rom rom;
+    rom.value = rom_value;
+    rom.rom_size = rom_size;
+    rom.map_offset = ROM_OFFSET;
+    mem_add_rom(&mem, &rom, "ROM");
+
+    Ram ram;
+    if (ram_value) {
+        ram.value = ram_value;
+        ram.size = ram_size;
+        ram.map_offset = RAM_OFFSET;
+        mem_add_ram(&mem, &ram, "RAM");
+    }
+
+    Cpu6502 cpu;
+    cpu.memmap = &mem;
+    cpu.addr_bus = rom.map_offset;
+    cpu_resb(&cpu);// just to do any internal setup before I mess around with all the registers
+    cpu.pc = ROM_OFFSET;
+    cpu.addr_bus = ROM_OFFSET;
+    cpu.x = rand() % 0xFF;
+    cpu.y = rand() % 0xFF;
+    cpu.a = rand() % 0xFF;
+    cpu.sp = rand() % 0xFF;
+    cpu.p = rand() % 0xFF;
+    // shouldn't matter, but that's why I'm rand()ing them
+    cpu.ir = rand() % 0xFF;
+    cpu.pd = rand() % 0xFF;
+    cpu.data_bus = rand() % 0xFF;
+
+    if (pre_execute) pre_execute(&cpu);
+
+    InstructionExecutionInfo info;
+    info.pc0 = cpu.pc;
+    info.x0 = cpu.x;
+    info.y0 = cpu.y;
+    info.a0 = cpu.a;
+    info.sp0 = cpu.sp;
+    info.p0 = cpu.p;
+
+    int cycles = 0;
+    do {
+        cpu_pulse(&cpu);
+        cycles++;
+    } while (cpu.tcu != 0 && cycles < MAX_CYCLES_PER_OP);
+
+    info.num_cycles = cycles;
+    info.pc1 = cpu.pc;
+    info.x1 = cpu.x;
+    info.y1 = cpu.y;
+    info.a1 = cpu.a;
+    info.sp1 = cpu.sp;
+    info.p1 = cpu.p;
+    info.bit_fields1 = cpu.bit_fields;
+    info.addr_bus1 = cpu.addr_bus;
+
+    return info;
+}
+
+TestResult compare_execution(
+    InstructionExecutionInfo actual,
+    ExpectedExecutionInfo expected)
+{
+    assert_equals(expected.num_cycles, actual.num_cycles, "Cycles");
+    assert_equals(
+        actual.pc0 + expected.instruction_size,
+        actual.pc1,
+        "Program Counter");
+    assert_equals(
+        actual.pc1,
+        actual.addr_bus1,
+        "Address Bus");
+    assert_equals(
+        PIN_READ,
+        actual.bit_fields1 & PIN_READ,
+        "R/W pin");
+
+    if (expected.updates_x) {
+        assert_equals(expected.x, actual.x1, "Register X");
+    } else {
+        assert_equals(actual.x0, actual.x1, "Register X (unchanged)");
+    }
+
+    if (expected.updates_y) {
+        assert_equals(expected.y, actual.y1, "Register Y");
+    } else {
+        assert_equals(actual.y0, actual.y1, "Register Y (unchanged)");
+    }
+
+    if (expected.updates_a) {
+        assert_equals(expected.a, actual.a1, "Register A");
+    } else {
+        assert_equals(actual.a0, actual.a1, "Register A (unchanged)");
+    }
+
+    if (expected.updates_sp) {
+        assert_equals(expected.sp, actual.sp1, "Register SP");
+    } else {
+        assert_equals(actual.sp0, actual.sp1, "Register SP (unchanged)");
+    }
+
+
+    if ((expected.flags_set & STAT_C_CARRY) == STAT_C_CARRY) {
+        assert_equals(STAT_C_CARRY, actual.p1 & STAT_C_CARRY, "Flag C");
+    } else if ((expected.flags_unset & STAT_C_CARRY) == STAT_C_CARRY) {
+        assert_equals(0, actual.p1 & STAT_C_CARRY, "Flag C");
+    } else {
+        assert_equals(actual.p0 & STAT_C_CARRY, actual.p1 & STAT_C_CARRY, "Flag C (unchanged)");
+    }
+
+    if ((expected.flags_set & STAT_Z_ZERO) == STAT_Z_ZERO) {
+        assert_equals(STAT_Z_ZERO, actual.p1 & STAT_Z_ZERO, "Flag Z");
+    } else if ((expected.flags_unset & STAT_Z_ZERO) == STAT_Z_ZERO) {
+        assert_equals(0, actual.p1 & STAT_Z_ZERO, "Flag Z");
+    } else {
+        assert_equals(actual.p0 & STAT_Z_ZERO, actual.p1 & STAT_Z_ZERO, "Flag Z (unchanged)");
+    }
+
+    if ((expected.flags_set & STAT_I_INTERRUPT) == STAT_I_INTERRUPT) {
+        assert_equals(STAT_I_INTERRUPT, actual.p1 & STAT_I_INTERRUPT, "Flag I");
+    } else if ((expected.flags_unset & STAT_I_INTERRUPT) == STAT_I_INTERRUPT) {
+        assert_equals(0, actual.p1 & STAT_I_INTERRUPT, "Flag I");
+    } else {
+        assert_equals(actual.p0 & STAT_I_INTERRUPT, actual.p1 & STAT_I_INTERRUPT, "Flag I (unchanged)");
+    }
+
+    if ((expected.flags_set & STAT_D_DECIMAL) == STAT_D_DECIMAL) {
+        assert_equals(STAT_D_DECIMAL, actual.p1 & STAT_D_DECIMAL, "Flag D");
+    } else if ((expected.flags_unset & STAT_D_DECIMAL) == STAT_D_DECIMAL) {
+        assert_equals(0, actual.p1 & STAT_D_DECIMAL, "Flag D");
+    } else {
+        assert_equals(actual.p0 & STAT_D_DECIMAL, actual.p1 & STAT_D_DECIMAL, "Flag D (unchanged)");
+    }
+
+    if ((expected.flags_set & STAT_B_BREAK) == STAT_B_BREAK) {
+        assert_equals(STAT_B_BREAK, actual.p1 & STAT_B_BREAK, "Flag B");
+    } else if ((expected.flags_unset & STAT_B_BREAK) == STAT_B_BREAK) {
+        assert_equals(0, actual.p1 & STAT_B_BREAK, "Flag B");
+    } else {
+        assert_equals(actual.p0 & STAT_B_BREAK, actual.p1 & STAT_B_BREAK, "Flag B (unchanged)");
+    }
+
+    if ((expected.flags_set & STAT___IGNORE) == STAT___IGNORE) {
+        assert_equals(STAT___IGNORE, actual.p1 & STAT___IGNORE, "Flag _");
+    } else if ((expected.flags_unset & STAT___IGNORE) == STAT___IGNORE) {
+        assert_equals(0, actual.p1 & STAT___IGNORE, "Flag _");
+    } else {
+        assert_equals(actual.p0 & STAT___IGNORE, actual.p1 & STAT___IGNORE, "Flag _ (unchanged)");
+    }
+
+    if ((expected.flags_set & STAT_V_OVERFLOW) == STAT_V_OVERFLOW) {
+        assert_equals(STAT_V_OVERFLOW, actual.p1 & STAT_V_OVERFLOW, "Flag V");
+    } else if ((expected.flags_unset & STAT_V_OVERFLOW) == STAT_V_OVERFLOW) {
+        assert_equals(0, actual.p1 & STAT_V_OVERFLOW, "Flag V");
+    } else {
+        assert_equals(actual.p0 & STAT_V_OVERFLOW, actual.p1 & STAT_V_OVERFLOW, "Flag V (unchanged)");
+    }
+
+    if ((expected.flags_set & STAT_N_NEGATIVE) == STAT_N_NEGATIVE) {
+        assert_equals(STAT_N_NEGATIVE, actual.p1 & STAT_N_NEGATIVE, "Flag N");
+    } else if ((expected.flags_unset & STAT_N_NEGATIVE) == STAT_N_NEGATIVE) {
+        assert_equals(0, actual.p1 & STAT_N_NEGATIVE, "Flag N");
+    } else {
+        assert_equals(actual.p0 & STAT_N_NEGATIVE, actual.p1 & STAT_N_NEGATIVE, "Flag N (unchanged)");
+    }
+
+    return (TestResult){is_success : true};
+}
 
 testcase(NOP_impl) {
     u8 rom_value[] = {
         (u8)0xEA,
     };
 
-    setup_cpu(rom_value);
-    cpu.x = 0;
-    cpu.y = 0;
-    cpu.a = 0;
-    cpu.sp = 0;
-    cpu.p = 0;
-
-    cpu_pulse(&cpu);
-    assert(cpu.tcu == 1, "Operation completed in < 2 cycles");
-    cpu_pulse(&cpu);
-    assert(cpu.tcu == 0, "Operation completed in > 2 cycles");
-    assert(cpu.x == 0, "Value of X register changed");
-    assert(cpu.y == 0, "Value of Y register changed");
-    assert(cpu.a == 0, "Value of Accumulator changed");
-    assert(cpu.p == 0, "Value of Status register changed");
-    assert(cpu.sp == 0, "Value of Stack Pointer changed");
-
-    return (TestResult) {
-        is_success : true,
-    };
+    return compare_execution(
+        execute_instruction(
+            rom_value, sizeof(rom_value) / sizeof(u8),
+            NULL, 0,
+            NULL),
+        (ExpectedExecutionInfo){
+            num_cycles: 2,
+            instruction_size: 1,
+        });
 }
-
-#define _LDX(rom_value, result_value, set_flags, unset_flags)                                          \
-    {                                                                                                  \
-        setup_cpu(rom_value);                                                                          \
-        cpu.x = 0;                                                                                     \
-        cpu.p &= ~(unset_flags);                                                                       \
-        cpu.p |= set_flags;                                                                            \
-                                                                                                       \
-        u8 p_expected = (cpu.p & ~(unset_flags)) | set_flags;                                          \
-        cpu_pulse(&cpu);                                                                               \
-        assert(cpu.tcu == 1, "Operation completed in < 2 cycles");                                     \
-        cpu_pulse(&cpu);                                                                               \
-        assert(cpu.tcu == 0, "Operation completed in > 2 cycles");                                     \
-        assert(cpu.x == result_value, "Value of X register incorrect");                                \
-        assert((cpu.p & STAT_N_NEGATIVE) == (p_expected & STAT_N_NEGATIVE), "N flag set incorrectly"); \
-        assert((cpu.p & STAT_Z_ZERO) == (p_expected & STAT_Z_ZERO), "Z flag set incorrectly");         \
-        assert(cpu.p == p_expected, "Unexpected status flags changed");                                \
-    }
 
 testcase(LDX_imm__N0Z0) {
     u8 rom_value[] = {
-        (u8)0xA2, (u8)0x1A,
+        (u8)0xA2, (u8)0x11,
     };
 
-    _LDX(rom_value, 0x1A, 0x00, STAT_N_NEGATIVE | STAT_Z_ZERO);
-
-    return (TestResult) {
-        is_success : true,
-    };
-}
-
-testcase(LDX_imm__N1Z0) {
-    u8 rom_value[] = {
-        (u8)0xA2, (u8)0x80,
-    };
-
-    _LDX(rom_value, 0x80, STAT_N_NEGATIVE, STAT_Z_ZERO);
-
-    return (TestResult) {
-        is_success : true,
-    };
+    return compare_execution(
+        execute_instruction(
+            rom_value, sizeof(rom_value) / sizeof(u8),
+            NULL, 0,
+            NULL),
+        (ExpectedExecutionInfo){
+            num_cycles: 2,
+            instruction_size: 2,
+            updates_x: true,
+            x: 0x11,
+            flags_unset: STAT_N_NEGATIVE | STAT_Z_ZERO,
+        });
 }
 
 testcase(LDX_imm__N0Z1) {
@@ -121,44 +292,40 @@ testcase(LDX_imm__N0Z1) {
         (u8)0xA2, (u8)0x00,
     };
 
-    _LDX(rom_value, 0x00, STAT_Z_ZERO, STAT_N_NEGATIVE);
-
-    return (TestResult) {
-        is_success : true,
-    };
+    return compare_execution(
+        execute_instruction(
+            rom_value, sizeof(rom_value) / sizeof(u8),
+            NULL, 0,
+            NULL),
+        (ExpectedExecutionInfo){
+            num_cycles: 2,
+            instruction_size: 2,
+            updates_x: true,
+            x: 0x00,
+            flags_set: STAT_Z_ZERO,
+            flags_unset: STAT_N_NEGATIVE,
+        });
 }
 
+testcase(LDX_imm__N1Z0) {
+    u8 rom_value[] = {
+        (u8)0xA2, (u8)0x81,
+    };
 
-    // Rom rom;
-    // if (!rom_load(&rom, ROM_FILE)) {
-    //     fprintf(stderr, "Failed parsing rom file.\n");
-    //     return 1;
-    // }
-    // mem_add_rom(&mem, &rom, "ROM");
-
-    // Ram zpg_ram;
-    // zpg_ram.map_offset = 0x0000;
-    // zpg_ram.size = 0x100;
-    // zpg_ram.value = malloc(zpg_ram.size);
-    // mem_add_ram(&mem, &zpg_ram, "ZPG");
-
-    // Ram stack_ram;
-    // stack_ram.map_offset = 0x0100;
-    // stack_ram.size = 0x0100;
-    // stack_ram.value = malloc(stack_ram.size);
-    // mem_add_ram(&mem, &stack_ram, "STACK");
-
-    // Ram ram;
-    // ram.map_offset = 0x0200;
-    // ram.size = 0x0600;
-    // ram.value = malloc(ram.size);
-    // mem_add_ram(&mem, &ram, "RAM");
-
-    // PPURegisters ppu;
-    // ppu.status = 0xA2;// just to make sure I can actually read from here
-    // mem_add_ppu(&mem, &ppu);
-
-
+    return compare_execution(
+        execute_instruction(
+            rom_value, sizeof(rom_value) / sizeof(u8),
+            NULL, 0,
+            NULL),
+        (ExpectedExecutionInfo){
+            num_cycles: 2,
+            instruction_size: 2,
+            updates_x: true,
+            x: 0x81,
+            flags_set: STAT_N_NEGATIVE,
+            flags_unset: STAT_Z_ZERO,
+        });
+}
 
 void get_test_name(char* buff, void *test_func) {
     void *bt[1] = { test_func };
@@ -186,8 +353,8 @@ int main() {
     testcase((*test_functions[])) = {
         &NOP_impl,
         &LDX_imm__N0Z0,
-        &LDX_imm__N1Z0,
         &LDX_imm__N0Z1,
+        &LDX_imm__N1Z0,
     };
 
     char buff[64];
@@ -199,7 +366,7 @@ int main() {
     {
         testcase((*test)) = test_functions[test_index];
         get_test_name(buff, test);
-        printf("Running test %i/%i '%s'...\r", test_index+1, n_tests, buff);
+        printf("Running test %i/%i '%s'...", test_index+1, n_tests, buff);
 
         clock_t start_test = clock();
         TestResult result = test();
@@ -208,17 +375,11 @@ int main() {
         int runtime_ms = (end_test - start_test) / CLOCKS_PER_SEC * 1000;
 
         if (result.is_success) {
-            printf("  %-20s Success (%ims)                             \n", buff, runtime_ms);
+            printf("\r  %-20s Success (%ims)                             \n", buff, runtime_ms);
         }
         else {
             all_success = false;
-            if (result.failed_message) {
-                printf("  %-20s Failed   (%ims) - %s                       \n", buff, runtime_ms, result.failed_message);
-                free(result.failed_message);
-            }
-            else {
-                printf("  %-20s Failed   (%ims)                            \n", buff, runtime_ms);
-            }
+            printf("\r  %-20s Failed  (%ims) - %s                       \n", buff, runtime_ms, error_message);
         }
     }
 
